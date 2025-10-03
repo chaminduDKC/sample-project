c2343e40337bba6bfbee464f12d21dd57732140f
ChaminduDKC <chamindud061@gmail.com>
2025-10-03 12:35:13 +0530

    added about route

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ app.get("/", (req, res)=>{
     res.send("Hello Main Branch")
 })
 
+app.get("/about", (req, res)=>{
+    res.send("About Page")
+})
+
 app.listen(PORT, ()=>{
     console.log("Server is running")
 })
